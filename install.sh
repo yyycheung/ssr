af4cce337dec4739eca047fd95b60a10208169d1
yyycheung <48355144+yyycheung@users.noreply.github.com>
2020-02-27 20:05:39 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get install python3
+apt-get install zip
+apt-get install vim
+mkdir shadowsocksr
+cd shadowsocksr/
+wget https://github.com/cndaqiang/shadowsocksr/archive/manyuser.zip
+unzip manyuser.zip
+cd shadowsocksr-manyuser/
